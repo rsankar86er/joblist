8c298f0375f07f77ec8c5a719083e6a971841dfd
rsankar86er <sankar86er@gmail.com>
2023-09-26 08:43:34 +0530

    Stepper Form Design Started

--- src/Components/JobCard.tsx
+++ src/Components/JobCard.tsx
@@ -2,13 +2,13 @@ import netfliximg from "../assets/images/netflix_logo.png";
 
 const JobCard = ({job}:any)=>{
     return(
-        <div className="card m-5 px-6 py-4 flex items-start">
+        <div className="card card-size m-5 px-6 py-4 flex items-start">
             <img alt="netflix logo" src={netfliximg}/>
             <div className="pl-2">
-                <h1>{job.job_title}</h1>
+                <h1 className="text-2xl">{job.job_title}</h1>
                 <p>{job.company_name} - {job.industry}</p>
-                <p>{job.location} ({job.remote_type})</p>
-                <p className="pt-6">Part-Time (9.00 am - 5.00 pm IST)</p>
+                <p className="placeholder">{job.location} ({job.remote_type})</p>
+                <p className="pt-6">Part-Time (9.00 am - 5.00 pm IST)</p> 
                 <p className="pt-2">Experience ({job.min_exp} - {job.max_exp} years)</p>
                 <p className="pt-2">INR (₹) {job.min_salary} - {job.max_salary} / Month</p>
                 <p className="pt-2">{job.total_employee} employees</p>
--- src/Components/StepperForm.tsx
+++ src/Components/StepperForm.tsx
@@ -1,8 +1,27 @@
 const StepperForm = ()=>{
     return(
         <div className="container flex justify-center">
-            <div className="card m-5">
-                StepperForm
+            <div className="card m-5 form-size p-8">
+                {/* Form Step - 1 */}
+                <div className="row flex justify-between">
+                    <h1 className="text-2xl">Create a job</h1>
+                    <span className="text-base font-medium">Step 1</span>
+                </div>
+                <div className="row">
+                    <label className="block w-full">Job Title</label>
+                    <input type="text" className="block w-full rounded-md grey-border p-1 placeholder:placeholder" placeholder=""></input>
+                </div>
+                <div className="row pt-6 pb-1">
+                    <label className="block w-full">Experience</label>
+                </div>
+                <div className="row flex">
+                    <div className="grow">
+                        <input type="text" className="block w-full rounded-md grey-border p-1 placeholder:placeholder" placeholder=""></input>
+                    </div>
+                    <div className="grow">
+                        <input type="text" className="block w-full rounded-md grey-border p-1 placeholder:placeholder"></input>
+                    </div>
+                </div>
             </div>
         </div>
     )
